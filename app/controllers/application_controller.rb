e9d4a46affd044e1f62de54ad28f177e17a9fc1e
lampv <lampvhy@gmail.com>
2018-03-28 16:34:32 +0700

    home_pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
+
+  private
+
+  def default_url_options options = {}
+    {locale: I18n.locale}.merge options
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(:locale)", :locale => /en|vi/ do
+    root "pages#home"
+    get "/*page" => "pages#show"
+  end
 end
